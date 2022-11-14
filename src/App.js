3ba8e38e0d1bc5282c4f55f8a53e30807499cad9
shubham-sanvariya <shubhamsanvariyait.com>
2022-11-14 21:18:40 +0530

    userList page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
      <Topbar />
     <div className="container">
       <Sidebar />
-      <Home />
+      <Routes>
+          <Route  path="/" element={<Home />}>     </Route>
+          <Route path="/users" element={<UserList />}></Route>
+      </Routes>
     </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -60,3 +60,86 @@ export const userData = [
         
     },
 ];
+
+export const userRows = [
+    {
+        id: 1,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 2,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 3,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 4,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 5,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 6,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 7,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 8,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 9,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 10,
+        username: 'Jon Snow',
+        avatar: "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
+        email: "Jon@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+];
